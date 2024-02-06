3e38525be6534807b5367c9137298ce7b6693510
Jamison <11932076+jamisonn@users.noreply.github.com>
2024-02-06 01:43:30 -0500

    Update main.py
    Updated code that removed the broken Youtube Video Links

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 import time
 import random
 import webbrowser
+import tkinter as tk
+
+# TodoSection
+
+window = tk.Tk()
 
 
 def ytNews():
-    webbrowser.open("https://www.youtube.com/watch?v=coYw-eVU0Ks&list=PLJnmPDzgaypH1G_v7N8iEO54b9F180rRhX9")
+    webbrowser.open("https://www.youtube.com/watch?v=SRS3oAwBivc&pp=ygUY5pel5pys6Kqe44Gu44OL44Ol44O844K5")
     time.sleep(180)
     os.system("taskkill /im msedge.exe /f")
 
@@ -24,33 +29,45 @@ def ytKids():
         webbrowser.open("https://www.youtube.com/watch?v=tcQn9kNGFuQ")
         time.sleep(180)
         os.system("taskkill /im msedge.exe /f")
+
+
 def ytSoapOpera():
     channel = random.randrange(0, 3)
     if channel == 0:
-        webbrowser.open("https://youtu.be/NPEoeMP3iNk")
+        webbrowser.open("https://www.youtube.com/watch?v=qlol2rW0pSk")
         time.sleep(180)
         os.system("taskkill /im msedge.exe /f")
     if channel == 1:
-        webbrowser.open("https://youtu.be/wakKSMXwzaQ")
+        webbrowser.open("https://www.youtube.com/watch?v=66vWPj9S7Fc&pp=ygUV5pel5pys6Kqe44Gu44OJ44Op44Oe")
         time.sleep(180)
         os.system("taskkill /im msedge.exe /f")
     if channel == 2:
-        webbrowser.open("https://youtu.be/cnPH__cr5t0")
+        webbrowser.open("https://www.youtube.com/watch?v=lYuc8RDaOdQ&pp=ygUV5pel5pys6Kqe44Gu44OJ44Op44Oe")
         time.sleep(180)
         os.system("taskkill /im msedge.exe /f")
 
 
-def main():
-    channel =random.randrange(0, 3)
+def begin_immersion():
+    channel = random.randrange(0, 3)
     if channel == 0:
         ytNews()
-        main()
+        begin_immersion()
     if channel == 1:
         ytKids()
-        main()
+        begin_immersion()
     if channel == 2:
         ytSoapOpera()
-        main()
+        begin_immersion()
 
 
-main()
+button = tk.Button(window, text="Begin Random Immersion", command=begin_immersion)
+button2 = tk.Button(window, text="Youtube Soap Opera", command=ytSoapOpera)
+button3 = tk.Button(window, text="Youtube Kids", command=ytKids)
+button4 = tk.Button(window, text="Youtube News ", command=ytNews)
+
+
+button.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+window.mainloop()
